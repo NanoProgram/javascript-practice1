1bac9652dc10e23c08d241385f6558ee80828bec
Daniel Perez <105433751+NanoProgram@users.noreply.github.com>
2024-05-16 21:34:18 +0000

    Exc 11

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,16 @@
 let total = prompt('How many km are left to go?');
 
 // Your code below:
+function kmToGo(total) {
+    let result;
+    if (total > 100) {
+        result = "We still have a bit of driving left to go";
+    } else if (total > 50 && total <= 100) {
+        result = "We'll be there in 5 minutes";
+    } else {
+        result = "I'm parking. I'll see you right now";
+    }
+    return result;
+}
+
+console.log(kmToGo(total));
